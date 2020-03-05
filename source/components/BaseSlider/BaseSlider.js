d2a49ba65851b22160e9ee95957ef30593509223
eugeny.f <fe@mstagency.ru>
2020-03-05 18:05:15 +0400

    feat accordion

---
+++ source/components/BaseSlider/BaseSlider.js
@@ -0,0 +1,9 @@
+import './BaseSlider.scss';
+
+var mySwiper = new Swiper ('.swiper-container', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerGroup: 1,
+  slidesPerView: 0,
+  spaceBetween: 10,
+})
--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,2 +1,5 @@
 import '../components/BaseHeader/BaseHeader';
 import '../components/BaseDesc/BaseDesc';
+import '../components/BaseAbout/BaseAbout';
+import '../components/BaseSlider/BaseSlider';
+import '../components/BaseQuestions/BaseQuestions';
