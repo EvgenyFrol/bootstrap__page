de8052bb9563ab6451d94089e5a7d4616583fd2f
eugeny.f <fe@mstagency.ru>
2020-03-12 17:59:09 +0400

    feat acc

--- source/components/BaseQuestions/BaseQuestions.js
+++ source/components/BaseQuestions/BaseQuestions.js
@@ -1,1 +1,11 @@
 import './BaseQuestions.scss';
+/* eslint-disable no-unused-vars */
+const accordion = document.querySelectorAll('.questions__header');
+const accbody = document.querySelectorAll('.questions__body');
+const card = document.querySelector('.questions__card');
+
+accordion.forEach((item) => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('questions__open');
+  })
+})
